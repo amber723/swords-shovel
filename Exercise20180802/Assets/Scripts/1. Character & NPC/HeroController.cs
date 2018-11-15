c6f9eebd19a6dd45ad9f69d8093b9472dc17fd82
amber723 <shu.xi@husky.neu.edu>
2018-11-16 00:09:38 +0800

    pursue and attack target

--- Exercise20180802/Assets/Scripts/1. Character & NPC/HeroController.cs
+++ Exercise20180802/Assets/Scripts/1. Character & NPC/HeroController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -8,6 +9,8 @@ public class HeroController : MonoBehaviour
     CharacterStats stats;
     Animator animator;
     NavMeshAgent agent;
+
+    GameObject attackTarget;
 
     void Awake()
     {
@@ -21,20 +24,66 @@ public class HeroController : MonoBehaviour
         animator.SetFloat("Speed", agent.velocity.magnitude);
     }
 
-    //public void SetDestination(Vector3 destination)
-    //{
-    //    agent.destination = destination;
-    //}
+    public void SetDestination(Vector3 destination)
+    {
+        StopAllCoroutines();
+        agent.isStopped = false;
+        agent.destination = destination;
+    }
 
     public void AttackTarget(GameObject target)
     {
-        var attack = demoAttack.CreateAttack(stats, target.GetComponent<CharacterStats>());
+        //var attack = demoAttack.CreateAttack(stats, target.GetComponent<CharacterStats>());
 
-        var attackables = target.GetComponentsInChildren(typeof(IAttackable));
+        //var attackables = target.GetComponentsInChildren(typeof(IAttackable));
 
-        foreach (IAttackable attackable in attackables)
+        //foreach (IAttackable attackable in attackables)
+        //{
+        //    attackable.OnAttack(gameObject, attack);
+        //}
+
+        var weapon = stats.GetCurrentWeapon();
+
+        if (weapon != null)
         {
-            attackable.OnAttack(gameObject, attack);
+            //interrupt any running PursueNAttack operations and allow us to
+            //switch target on the fly.
+            StopAllCoroutines();
+
+            agent.isStopped = false;
+            attackTarget = target;
+
+            StartCoroutine(PursueNAttackTarget());
+        }
+    }
+
+    //running towards the target unitl it's within range.
+    IEnumerator PursueNAttackTarget()
+    {
+        agent.isStopped = false;
+        var weapon = stats.GetCurrentWeapon();
+
+        while (Vector3.Distance(transform.position, attackTarget.transform.position) 
+            > weapon.Range)
+        {
+            agent.destination = attackTarget.transform.position;
+            yield return null;
+        }
+
+        agent.isStopped = true;
+
+        //make sure facing the target
+        transform.LookAt(attackTarget.transform);
+        animator.SetTrigger("Attack");
+    }
+
+    //To be called by the animation controller
+    public void Hit()
+    {
+        //Have our weapon attack the attack target
+        if (attackTarget != null)
+        {
+            stats.GetCurrentWeapon().ExecuteAttack(gameObject, attackTarget);
         }
     }
 }
